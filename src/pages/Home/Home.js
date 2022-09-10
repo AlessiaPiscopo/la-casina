36c586f3f321f2bdcdea25d9c14de237c4b5c02f
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-09-10 11:49:01 -0400

    Style background video and fix controls display

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,61 +1,35 @@
-import { StyledHome } from "./Home.styled";
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from "react-responsive-carousel";
+import {
+  StyledHome,
+  StyledBackgroundVideo,
+  ContentContainer,
+  Logo,
+} from "./Home.styled";
 import PromoStill1 from "../../assets/promo-still-1.png";
-// import PromoStill2 from "../../assets/promo-still-2.png";
-// import PromoStill3 from "../../assets/promo-still-3.png";
-// import ImageCarousel from "./ImageCarousel";
+import LogoSvg from "../../assets/logo.svg";
 
 import CasinaVideo from "./promo-video-cut.mp4";
 
 const Home = () => {
   return (
     <StyledHome>
-      <div>Home page</div>
-      {/* <Carousel /> */}
-
-      {/* <ImageCarousel /> */}
-
-      {/* <video controls autostart autoPlay src={CasinaVideo} type="video/mp4" /> */}
-
-      <video id="background-video" autoplay loop muted poster={PromoStill1}>
+      <StyledBackgroundVideo
+        id="background-video"
+        autostart
+        // autoPlay
+        loop
+        muted
+        disablePictureInPicture
+        controlsList="nodownload"
+        poster={PromoStill1}
+      >
         <source src={CasinaVideo} type="video/mp4" />
-      </video>
+      </StyledBackgroundVideo>
+
+      <Logo src={LogoSvg} alt="La Casina logo" />
+      <h1>hello</h1>
+      <h2>some test text</h2>
     </StyledHome>
   );
 };
-
-// class DemoCarousel extends Component {
-//   render() {
-//     return (
-//       <Carousel>
-//         <div>
-//           <img src={PromoStill1} />
-//           <p className="legend">Legend 1</p>
-//         </div>
-//         <div>
-//           <img src={PromoStill2} />
-//           <p className="legend">Legend 2</p>
-//         </div>
-//         <div>
-//           <img src={PromoStill3} />
-//           <p className="legend">Legend 3</p>
-//         </div>
-//       </Carousel>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<DemoCarousel />, document.querySelector(".demo-carousel"));
-
-// Don't forget to include the css in your page
-
-// Using webpack or parcel with a style loader
-// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-// Using html tag:
-// <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
 
 export default Home;
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -2,16 +2,44 @@ import styled from "styled-components";
 
 export const StyledHome = styled.main`
   width: 100vw;
+  height: 100vh;
+
+  h1,
+  h2 {
+    color: white;
+    font-family: Trebuchet MS;
+    font-weight: bold;
+    text-align: center;
+    z-index: 11;
+  }
+
+  h1 {
+    font-size: 6rem;
+    margin-top: 30vh;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
 `;
 
-// export const StyledBackgroundVideo = styled.video`
-//   width: 100vw;
-//   height: 100vh;
-//   object-fit: cover;
-//   position: fixed;
-//   left: 0;
-//   right: 0;
-//   top: 0;
-//   bottom: 0;
-//   z-index: 10;
-// `;
+export const StyledBackgroundVideo = styled.video`
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -10;
+  filter: brightness(50%);
+`;
+
+export const ContentContainer = styled.div`
+  border: 2px solid red;
+`;
+
+export const Logo = styled.img`
+  margin: auto 0;
+`;
